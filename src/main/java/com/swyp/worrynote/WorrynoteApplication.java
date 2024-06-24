4a9cd3f5796deef0f53790ea3f46b4f03ba1a619
chanyong <jcy168942@gmail.com>
2024-06-24 17:27:09 +0900

    init

---
+++ src/main/java/com/swyp/worrynote/WorrynoteApplication.java
@@ -0,0 +1,13 @@
+package com.swyp.worrynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorrynoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorrynoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swyp/worrynote/WorrynoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyp.worrynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorrynoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
